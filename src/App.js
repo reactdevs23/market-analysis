357c82df6a6a3df9867113b3c6810ee6312915b8
dalimkumar604 <dalimkumar212582@gmail.com>
2024-02-01 10:22:27 +0600

    dasfas

--- src/App.js
+++ src/App.js
@@ -1,48 +1,62 @@
+import { useState } from "react";
 import MainComponent from "./components/MaiComponent/MainComponent";
 import { icon } from "./images";
 
 function App() {
-  const allData = {
+  const [allData, setAllData] = useState({
     fontFamily: "'Roboto', sans-serif",
-    primaryColor: "#000",
-    secondaryColor: "#fff",
-
+    bgColor: "#fff",
+    textColor: "#000",
+    numberColor: "#000",
+    borderColor: "#000",
     heading: "The methods of <br/> <span>Market Analysis</span>",
     data: [
       {
         icon: icon,
-        title: "Analysis of Factors",
+        title: "Analysis of Factors ",
         info: `There are many companies in today's market that have "maskots" who act as icons for there overall brand image`,
-        bg: "#8A6AD8",
+        headerBg: "#8A6AD8",
+        headerIconColor: "#000",
+        shadowColor: "#000",
       },
       {
         icon: icon,
 
-        title: "Analysis of Clusters",
-        info: `There are many companies in today's market that have "maskots" who act as icons `,
+        title: "Analysis of Clusters  ",
+        info: `There are many companies in today's  market that have "maskots" who act as icons `,
 
-        bg: "#FF9772",
+        headerBg: "#FF9772",
+        headerIconColor: "#000",
+        shadowColor: "#000",
       },
       {
         icon: icon,
 
         title: "Analysis Log Regression",
         info: `There are many companies in today's market that have "maskots" who act as icons for there overall brand image`,
-        bg: "#8A6AD8",
+        headerBg: "#8A6AD8",
+        headerIconColor: "#000",
+        shadowColor: "#000",
       },
       {
         icon: icon,
         title: "Discrimination Analysis",
         info: `There are many companies in today's market that have "maskots" who act as icons for there overall brand image`,
-        bg: "#FF9772",
+        headerBg: "#FF9772",
+        headerIconColor: "#000",
+        shadowColor: "#000",
       },
     ],
-  };
+  });
   return (
     <div
       style={{
-        "--primaryColor": allData.primaryColor,
-        "--secondaryColor": allData.secondaryColor,
+        "--bgColor": allData.bgColor,
+
+        "--textColor": allData.textColor,
+        "--numberColor": allData.numberColor,
+        "--borderColor": allData.borderColor,
+
         fontFamily: allData.fontFamily,
       }}
     >
--- src/components/MaiComponent/MainComponent.js
+++ src/components/MaiComponent/MainComponent.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import styles from "./MainComponent.module.css";
 
 const MainComponent = ({ heading, data }) => {
+  const titleRefs = useRef([]);
+
+  useEffect(() => {
+    // Get the maximum height among all title elements
+    const titles = titleRefs.current.map(
+      (ref) => ref.getBoundingClientRect().height
+    );
+    const maxTitleHeight = Math.max(...titles);
+
+    // Set the maximum height to all title elements using the titleRefs
+    titleRefs.current.forEach((ref) => {
+      ref.style.height = `${maxTitleHeight}px`;
+    });
+  }, [data]);
+
   return (
     <div className={styles.mainWrapper}>
       <div className={styles.wrapper}>
@@ -13,13 +28,20 @@ const MainComponent = ({ heading, data }) => {
         <div className={styles.cards}>
           {data.map((el, id) => (
             <div
-              style={{ "--bg": el.bg }}
+              style={{
+                "--bg": el.headerBg,
+                "--shadowColor": el.shadowColor,
+                "--headerIconColor": el.headerIconColor,
+              }}
               key={id}
               className={`${styles.card} ${styles[`items-${data.length}`]}  ${
                 data.length > 5 && styles[`items-5`]
               }`}
             >
-              <div className={styles.header} style={{ background: el.bg }}>
+              <div
+                className={styles.header}
+                style={{ background: el.headerBg }}
+              >
                 <span className={styles.underline}>_</span>
                 <i className="fa-regular fa-square"></i>
                 <i className="fa-solid fa-xmark"></i>
@@ -29,8 +51,19 @@ const MainComponent = ({ heading, data }) => {
                 {id + 1 < 10 ? `0${id + 1}` : id + 1}.
               </p>
 
-              <h4 className={styles.title}>{el.title}</h4>
-              <p className={styles.info}>{el.info}</p>
+              <div className={styles.titleContainer}>
+                <h4
+                  ref={(el) => (titleRefs.current[id] = el)}
+                  className={styles.title}
+                >
+                  {el.title}
+                </h4>
+              </div>
+
+              <div className={styles.infoContainer}>
+                <p className={styles.info}>{el.info}</p>
+              </div>
+
               <div className={styles.iconContainer}>
                 <img src={el.icon} alt="#" className={styles.icon} />
               </div>
